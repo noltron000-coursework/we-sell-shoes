1739d9138603e4aaf3c9ca23fe49d07912adc314
Nolan Kovacik <noltron000@gmail.com>
2018-08-28 14:27:31 -0700

    module almost complete. But not complete yet.

--- script.js
+++ script.js
@@ -135,3 +135,41 @@ for (let i = 0; i < acc.length; i++) {
 		}
 	});
 }
+
+
+
+
+
+/////////popup//////////////
+
+//declare default vars
+const popUp = document.getElementById('pop-up');
+const popUpLayer = document.getElementById('pop-up-layer');
+const closeModal = document.getElementById('close-pop-up');
+
+//nav-bar button
+const subBtn = document.getElementById('sub-btn');
+
+
+
+//navbar button click makes the popup appear
+subBtn.addEventListener("click", () => {
+  popUpLayer.style.display = 'flex';
+})
+
+//close model click makes the popup disappear
+closeModal.addEventListener("click", function() {
+  popUpLayer.style.display = 'none';
+})
+
+let popUpSeconds = 1;
+
+//trigger popup after seconds variable
+let popUpFun = window.setInterval(function() {
+  popUpLayer.style.display = 'flex';
+
+  window.clearInterval(popUpFun);
+
+}, popUpSeconds * 1000);
+
+///////////////////
