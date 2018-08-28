c861a2593e00b250c3194e1c3f4105a70c11dc21
noltron000 <noltron000@gmail.com>
2018-08-28 14:37:39 -0700

    testing

--- script.js
+++ script.js
@@ -154,21 +154,21 @@ const subBtn = document.getElementById('sub-btn');
 
 //navbar button click makes the popup appear
 subBtn.addEventListener("click", () => {
-  popUpLayer.style.display = 'flex';
+	popUpLayer.style.display = 'flex';
 })
 
 //close model click makes the popup disappear
 closeModal.addEventListener("click", function() {
-  popUpLayer.style.display = 'none';
+	popUpLayer.style.display = 'none';
 })
 
 let popUpSeconds = 1;
 
 //trigger popup after seconds variable
 let popUpFun = window.setInterval(function() {
-  popUpLayer.style.display = 'flex';
+	popUpLayer.style.display = 'flex';
 
-  window.clearInterval(popUpFun);
+	window.clearInterval(popUpFun);
 
 }, popUpSeconds * 1000);
 
